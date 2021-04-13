f9887b66ed28ef90e788a63f210f807098be36d1
StarmasterK13 <69331111+StarmasterK13@users.noreply.github.com>
2021-04-13 14:10:45 -0400

    Interaction System/Tile Functionality Preparations (#8)
    * Tile Functionality Preparations
    
    Tile interaction system, movement system overhaul, misc. tile functionality preparations, red/blue wall functionality, and red/blue crate functionality
    
    * Added comments
    
    Co-authored-by: MacroMew <33503006+MacroMew@users.noreply.github.com>

--- scripts/main.py
+++ scripts/main.py
@@ -23,6 +23,9 @@ FPS = gamedata.fps
 lm = gamedata.levelmanager
 current_level = 0
 moves = 0
+red_on = False
+blue_on = False
+moving_state = 0
 # create game loop
 
 running = True
@@ -74,29 +77,39 @@ while running:
         # move player
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                moves += 1
-                lm.level_list[current_level].detection("up", 0, 0 - lm.level_list[current_level].tile_size, screen_size)
-            if event.key == pygame.K_DOWN:
-                lm.level_list[current_level].detection("down", 0, lm.level_list[current_level].tile_size, screen_size)
-                moves += 1
-            if event.key == pygame.K_LEFT:
-                lm.level_list[current_level].detection("left", 0 - lm.level_list[current_level].tile_size, 0, screen_size)
-                moves += 1
-            if event.key == pygame.K_RIGHT:
-                lm.level_list[current_level].detection("right", lm.level_list[current_level].tile_size, 0, screen_size)
-                moves += 1
-            if event.key == pygame.K_u and moves > 0:
-                lm.level_list[current_level].undo()
-                moves -= 1
-            if event.key == pygame.K_r:
-                lm.level_list[current_level].restart(moves)
-                moves = 0
-            if event.key == pygame.K_d:
-                print(lm.level_list[current_level].objects[1].position_history)
-                print(moves)
             if event.key == pygame.K_ESCAPE:
                 running = False
+            if moving_state == 0:
+                if event.key == pygame.K_r:
+                    lm.level_list[current_level].restart(moves)
+                    moves = 0
+                if event.key == pygame.K_u and moves > 0:
+                    lm.level_list[current_level].undo()
+                    moves -= 1
+                if event.key == pygame.K_UP:
+                    lm.level_list[current_level].objects[0].push("up", 1)
+                if event.key == pygame.K_DOWN:
+                    lm.level_list[current_level].objects[0].push("down", 1)
+                if event.key == pygame.K_LEFT:
+                    lm.level_list[current_level].objects[0].push("left", 1)
+                if event.key == pygame.K_RIGHT:
+                    lm.level_list[current_level].objects[0].push("right", 1)
+            if event.key == pygame.K_d:
+                #debugging command, remove in final build
+                print(lm.level_list[current_level].objects[1].position_history)
+                print(moves)
+            if event.key == pygame.K_e:
+                #debugging command, remove in final build
+                if red_on:
+                    red_on = False
+                else:
+                    red_on = True
+            if event.key == pygame.K_b:
+                #debugging command, remove in final build
+                if blue_on:
+                    blue_on = False
+                else:
+                    blue_on = True
             if event.key == pygame.K_1:
                 current_level = lm.level_list[0]
             elif event.key == pygame.K_2:
@@ -112,22 +125,76 @@ while running:
             elif event.key == pygame.K_7:
                 current_level = lm.level_list[6]
 
-        # draw new screen
+    # address interactions
 
-        screen.blit(background, (0, 0))
+    objects_moving = 0
+    #checks which movement state the game is in (0 = nothing moving, 1 = on tiles but stuff still needs to move, 2 = currently moving)
+    for item in lm.level_list[current_level].objects:
+        if len(item.push_requests) > 0:
+            if item.push_requests[0][1] % 5 == 0:
+                moving_state = 1
+            else:
+                moving_state = 2
+            objects_moving += 1
+    if objects_moving == 0:
+        if moving_state != 0:
+            #triggers the end of the turn (updates position/state histories)
+            for item in lm.level_list[current_level].tiles:
+                item.turn_end()
+            for item in lm.level_list[current_level].objects:
+                item.turn_end()
+            moves += 1
+        moving_state = 0
+    if moving_state == 1:
+        #triggers movement detection and execution
+        lm.level_list[current_level].move_detection(screen_size)
+        lm.level_list[current_level].move_cycle()
+        #activates levers, buttons, or rotators if something is on top of them
+        for item in lm.level_list[current_level].tiles:
+            if item.__class__ == levels.levelclass.tileclass.Lever or item.__class__ == levels.levelclass.tileclass.Button or item.__class__ == levels.levelclass.tileclass.Rotator:
+                item.detect(lm.level_list[current_level].objects)
+        #swaps the red and/or blue state from any lever or button interactions
+        for item in lm.level_list[current_level].tiles:
+            if item.__class__ == levels.levelclass.tileclass.Lever or item.__class__ == levels.levelclass.tileclass.Button:
+                if item.red_swap():
+                    if red_on:
+                        red_on = False
+                    else:
+                        red_on = True
+                if item.blue_swap():
+                    if blue_on:
+                        blue_on = False
+                    else:
+                        blue_on = True
+        #updates the crates and walls depending on which colors are on or off and tells objects to be pushed if they're on an arrow
+        for item in lm.level_list[current_level].objects:
+            if item.__class__ == levels.levelclass.objectclass.Crate:
+                item.update_state(red_on, blue_on)
+        for item in lm.level_list[current_level].tiles:
+            if item.__class__ == levels.levelclass.tileclass.Wall:
+                item.update_state(red_on, blue_on)
+            if item.__class__ == levels.levelclass.tileclass.Arrow:
+                item.push_objects(lm.level_list[current_level].objects)
+    elif moving_state == 2:
+        #moves objects if they're currently moving in between tiles
+        lm.level_list[current_level].move_cycle()
 
-        # update the level to the screen
+    # draw new screen
+            
+    screen.blit(background, (0, 0))
 
-        for i in range(len(lm.level_list[current_level].tiles)):
-            screen.blit(pygame.transform.scale(lm.level_list[current_level].tiles[i].surface,
-                                              (math.ceil(lm.level_list[current_level].tile_size), math.ceil(lm.level_list[current_level].tile_size))),
-                       (round(lm.level_list[current_level].tiles[i].x), round(lm.level_list[current_level].tiles[i].y)))
-        for i in range(len(lm.level_list[current_level].objects)):
-            screen.blit(pygame.transform.scale(lm.level_list[current_level].objects[i].surface,
-                                              (math.ceil(lm.level_list[current_level].tile_size), math.ceil(lm.level_list[current_level].tile_size))),
-                       (round(lm.level_list[current_level].objects[i].x), round(lm.level_list[current_level].objects[i].y)))
+    # update the level to the screen
 
-        clock.tick(FPS)
-        pygame.display.flip()
+    for i in range(len(lm.level_list[current_level].tiles)):
+        screen.blit(pygame.transform.scale(lm.level_list[current_level].tiles[i].surface,
+                                            (math.ceil(lm.level_list[current_level].tile_size), math.ceil(lm.level_list[current_level].tile_size))),
+                    (round(lm.level_list[current_level].tiles[i].x), round(lm.level_list[current_level].tiles[i].y)))
+    for i in range(len(lm.level_list[current_level].objects)):
+        screen.blit(pygame.transform.scale(lm.level_list[current_level].objects[i].surface,
+                                            (math.ceil(lm.level_list[current_level].tile_size), math.ceil(lm.level_list[current_level].tile_size))),
+                    (round(lm.level_list[current_level].objects[i].x), round(lm.level_list[current_level].objects[i].y)))
+
+    clock.tick(FPS)
+    pygame.display.flip()
 
 pygame.quit()
--- scripts/tileclass.py
+++ scripts/tileclass.py
@@ -12,21 +12,26 @@ button = pygame.image.load('../images/button.png')
 arrow = pygame.image.load('../images/right1.png')
 rotator = pygame.image.load('../images/tile_clockwise.png')
 void = pygame.image.load('../images/void.png')
+red_block = pygame.image.load('../images/red_wall.png')
+blue_block = pygame.image.load('../images/blue_wall.png')
+red_block_on = pygame.image.load('../images/red_floor.png')
+blue_block_on = pygame.image.load('../images/blue_floor.png')
 
-tileset = [pit, floor, wall, lever, button, arrow, rotator, goal, spawn, void]
-nameset = ["pit", "floor", "wall", "lever", "button", "arrow", "rotator", "goal", "spawn", "void"]
+tileset = [pit, floor, wall, lever, button, arrow, rotator, goal, spawn, void, red_block, blue_block]
+nameset = ["pit", "floor", "wall", "lever", "button", "arrow", "rotator", "goal", "spawn", "void", "red_block", "blue_block"]
 
 # variable to access player object
 
 
 class Tile(object):
-    def __init__(self, x, y, variant):
+    def __init__(self, x, y, variant, state):
         # creates a tile (variant is what type of tile it is, see the tileset list for info)
         self.x = x
         self.y = y
         self.surface = tileset[variant]
         self.name = nameset[variant]
-        self.state_history = []
+        self.state = state
+        self.state_history = [state]
         # the list above will be used for reverting changes to the states of tiles after an undo or
         # restart is triggered once tile functionality and different states are implemented
 
@@ -37,24 +42,39 @@ class Tile(object):
             self.is_floor = True
         if variant == 0:
             self.__class__ = Pit
-        if variant == 1:
+        elif variant == 1:
             self.__class__ = Floor
-        if variant == 2:
+        elif variant == 2 or variant == 10 or variant == 11:
             self.__class__ = Wall
-        if variant == 3:
+        elif variant == 3:
             self.__class__ = Lever
-        if variant == 4:
+        elif variant == 4:
             self.__class__ = Button
-        if variant == 5:
+        elif variant == 5:
             self.__class__ = Arrow
-        if variant == 6:
+        elif variant == 6:
             self.__class__ = Rotator
-        if variant == 7:
+        elif variant == 7:
             self.__class__ = Goal
-        if variant == 8:
+        elif variant == 8:
             self.__class__ = Spawn
-        if variant == 9:
+        elif variant == 9:
             self.__class__ = Void
+    
+    def back(self, back_num):
+        #reverts the tile's state to a previous one in the state history
+        self.state = self.state_history[len(self.state_history) - back_num - 1]
+        for i in range(back_num):
+            self.state_history.pop(-1)
+            
+    def turn_end(self):
+        self.state_history.append(self.state)
+                
+    # create action funciton
+    # will be overwritten in subclasses for those with actions
+    # return TypeException if the superclass function runs instead of subclass function
+    def action(self):
+        return  # figure out how to return a type exception
 
     # create action funciton
     # will be overwritten in subclasses for those with actions
@@ -66,7 +86,7 @@ class Tile(object):
 class Pit(Tile):
     def __init__(self):
         # creates a pit subclass
-        super().__init__()
+        super().__init__()        
 
     def action(self):
         """
@@ -102,6 +122,33 @@ class Wall(Tile):
         """
         return
 
+    def update_state(self, red_on, blue_on):
+        #updates how the wall behaves depending on which colors are on or off
+        if self.name == "red_block":
+            if red_on:
+                self.state = 1
+                self.is_floor = True
+                self.surface = red_block_on
+            else:
+                self.state = 0
+                self.is_floor = False
+                self.surface = red_block
+        elif self.name == "blue_block":
+            if blue_on:
+                self.state = 1
+                self.is_floor = True
+                self.surface = blue_block_on
+            else:
+                self.state = 0
+                self.is_floor = False
+                self.surface = blue_block
+            
+    def action(self):
+        """
+        :return:
+        """
+        return
+
 
 class Goal(Tile):
     def __init__(self):
@@ -121,6 +168,35 @@ class Lever(Tile):
         super().__init__()
         # variable that tells whether this tile is red, blue, or no color (grey)
         self.type = "grey"
+
+
+    def detect(self, objects):
+        #detects if an object is on top of it (0 is no object, 1 is object, 2 is object but the color swap has already been triggered)
+        detector = 0
+        for item in objects:
+            if round(item.x) == round(self.x) and round(item.y) == round(self.y):
+                detector = 1
+                if self.state == 0:
+                    self.state = 1
+        if detector == 0:
+            self.state = 0
+
+    def red_swap(self):
+        #returns a boolean that will swap if red is on or off depending on if something triggered the lever
+        if (self.type == "red" or self.type == "grey") and self.state == 1:
+            if self.type == "red":
+                self.state = 2
+            return True
+        else:
+            return False
+
+    def blue_swap(self):
+        #returns a boolean that will swap if blue is on or off depending on if something triggered the lever
+        if (self.type == "blue" or self.type == "grey") and self.state == 1:
+            self.state = 2
+            return True
+        else:
+            return False
 
     def action(self):
         """
@@ -153,6 +229,36 @@ class Button(Tile):
         # variable that tells whether this tile is red, blue, or no color (grey)
         self.type = "grey"
 
+    def detect(self, objects):
+        #detects if an object is on top of it (0 is no object, 1 is object, 2 is object but the color swap has already been triggered)
+        detector = 0
+        for item in objects:
+            if round(item.x) == round(self.x) and round(item.y) == round(self.y):
+                detector = 1
+                if self.state == 0:
+                    self.state = 1
+        if detector == 0 and self.state == 2:
+            self.state = 1
+        elif detector == 0:
+            self.state = 0
+
+    def red_swap(self):
+        #returns a boolean that will swap if red is on or off depending on if something got on or off the button
+        if (self.type == "red" or self.type == "grey") and self.state == 1:
+            if self.type == "red":
+                self.state = 2
+            return True
+        else:
+            return False
+
+    def blue_swap(self):
+        #returns a boolean that will swap if blue is on or off depending on if something got on or off the button
+        if (self.type == "blue" or self.type == "grey") and self.state == 1:
+            self.state = 2
+            return True
+        else:
+            return False
+        
     def action(self):
         """
         :return:
@@ -182,6 +288,17 @@ class Rotator(Tile):
         # creates an arrow rotator subclass (includes all variants)
         super().__init__()
 
+    def detect(self, objects):
+        detector = 0
+        for item in objects:
+            if round(item.x) == round(self.x) and round(item.y) == round(self.y):
+                detector = 1
+        if detector == 1 and self.state == 0:
+            self.state = 1
+            #put code that checks for adjacent arrows and changes their direction here
+        elif detector == 0:
+            self.state = 0
+                
     def action(self):
         """
         :return:
@@ -193,6 +310,10 @@ class Arrow(Tile):
     def __init__(self):
         # creates a pushing arrow subclass (includes all variants)
         super().__init__()
+
+    def push_objects(self, objects):
+        #put code that checks for objects on the arrow and gives them an appropriate push request here
+        return
 
     def action(self):
         """
